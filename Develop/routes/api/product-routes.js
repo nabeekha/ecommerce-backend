4a765eae209bc855d8c01c6c8f75855882ac821f
nabeekha <nabeekha@umich.edu>
2023-04-24 21:47:33 -0400

    commit with updated routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const category = await Category.findAll();
-    res.status(200).json(category);
+    const product = await Product.findAll();
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -95,8 +95,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const category = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -67,8 +67,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
